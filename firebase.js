acc6c3242735be2cb13677e231d69e7d3a1885f2
anisimaa <isla.anisimaa00@gmail.com>
2025-02-27 10:56:43 +0200

    firebase alterations

--- firebase.js
+++ firebase.js
@@ -3,13 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"; 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDIxngHAdOMbrnOXnr0Lv3LTmHnxrajs4o",
-  authDomain: "locationapp-22332.firebaseapp.com",
-  projectId: "locationapp-22332",
-  storageBucket: "locationapp-22332.firebasestorage.app",
-  messagingSenderId: "589824281968",
-  appId: "1:589824281968:web:6ab8ddbf3e70567f9b93b5",
-  measurementId: "G-HFD8BYC30R"
+
 };
 
 const app = initializeApp(firebaseConfig);
